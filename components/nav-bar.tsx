476d7d70cca42acd90d5cafaa114486809ca1b8d
sf <stan0130@student.monash.edu>
2026-02-06 04:59:39 +0800

    responsive nav bar

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,36 +1,57 @@
+"use client";
+import { motion, useMotionValueEvent, useScroll } from "motion/react";
+import React, { useState, useEffect, useCallback } from "react";
+
 export default function NavBar() {
+  const { scrollY } = useScroll();
+  const [ hidden, setHidden ] = useState(false);
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    const previous = scrollY.getPrevious();
+    if (previous && latest > previous && latest > 150) {
+      setHidden(true);
+    } else {
+      setHidden(false);
+    }
+  });
   return (
     <>
-      <header className="sticky top-0 z-50">
-        <nav className="relative flex items-center justify-between p-6 lg:px-8">
-          <div className="flex gap-x-12 items-center justify-center mx-auto w-screen">
-            <a
-              href="#"
-              className="text-sm/6 font-semibold text-gray-900 dark:text-white"
-            >
-              Product
-            </a>
-            <a
-              href="#"
-              className="text-sm/6 font-semibold text-gray-900 dark:text-white"
-            >
-              Features
-            </a>
-            <a
-              href="#"
-              className="text-sm/6 font-semibold text-gray-900 dark:text-white"
-            >
-              Marketplace
-            </a>
-            <a
-              href="#"
-              className="text-sm/6 font-semibold text-gray-900 dark:text-white"
-            >
-              Company
-            </a>
-          </div>
-        </nav>
-      </header>
+      <motion.nav
+        variants={{
+          visible: { y: 0 },
+          hidden: { y: "-100%" },
+        }}
+        animate={hidden ? "hidden" : "visible"}
+        transition={{ duration: 0.35, ease: "easeInOut" }}
+        className="sticky top-0 z-100 scroll flex items-center justify-between p-6 lg:px-8 bg-gray-900" 
+      >
+        <div className="flex gap-x-12 items-center justify-center mx-auto w-screen">
+          <a
+            href="#"
+            className="text-sm/6 font-semibold text-gray-900 dark:text-white"
+          >
+            About me
+          </a>
+          <a
+            href="#experience"
+            className="text-sm/6 font-semibold text-gray-900 dark:text-white"
+          >
+            Experience
+          </a>
+          <a
+            href="#projects"
+            className="text-sm/6 font-semibold text-gray-900 dark:text-white"
+          >
+            Projects
+          </a>
+          <a
+            href="#connect"
+            className="text-sm/6 font-semibold text-gray-900 dark:text-white"
+          >
+            Connect
+          </a>
+        </div>
+      </motion.nav>
     </>
-  )
+  );
 }
